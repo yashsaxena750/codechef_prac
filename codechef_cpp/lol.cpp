9adca891f90f61ac02dc4c0c4fe9cc460b4307e3
yashsaxena750 <yashsaxena986@gmail.com>
2024-01-17 00:24:05 +0530

    worked with .hpp files

---
+++ codechef_cpp/lol.cpp
@@ -0,0 +1,8 @@
+//
+//  lol.cpp
+//  codechef_cpp
+//
+//  Created by Yash Saxena on 17/01/24.
+//
+
+#include "lol.hpp"
---
+++ codechef_cpp/lol.hpp
@@ -0,0 +1,13 @@
+//
+//  lol.hpp
+//  codechef_cpp
+//
+//  Created by Yash Saxena on 17/01/24.
+//
+
+#ifndef lol_hpp
+#define lol_hpp
+
+#include <stdio.h>
+
+#endif /* lol_hpp */
